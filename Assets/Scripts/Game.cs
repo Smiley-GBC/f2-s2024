b83b540f3784b6ce456383aa0f353e09e8720301
Smiley98 <smileysofficialcinema@gmail.com>
2024-06-13 11:09:22 -0400

    Added timer

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 // Class for cross-communication between systems -- Ship, Bullet, Asteroid, etc.
 public class Game : MonoBehaviour
 {
+    Timer timer = new Timer();
+
     void Start()
     {
+        timer.total = 0.25f;
         //AudioManager.PlayMusic(MusicName.WINGS);
     }
 
     void Update()
     {
+        // Run the test function every 0.25 seconds!
+        timer.Tick(Time.deltaTime);
+        if (timer.Expired())
+        {
+            timer.Reset();
+            //Test();
+        }
+    }
 
+    void Test()
+    {
+        Debug.Log("Test");
     }
 }
