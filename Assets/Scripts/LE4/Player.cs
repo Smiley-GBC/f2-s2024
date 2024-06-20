e46ff6d4d0eeb91333288f1ca9894923c33dff45
Smiley98 <smileysofficialcinema@gmail.com>
2024-06-20 11:20:39 -0400

    Added jump limit (week 7 complete)

--- Assets/Scripts/LE4/Player.cs
+++ Assets/Scripts/LE4/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     const float xSpeedMax = 10.0f;
     const float ySpeedMax = 10.0f;
 
+    const int jumpCount = 2;
+    int jumps = jumpCount;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -29,20 +32,20 @@ public class Player : MonoBehaviour
             rb.AddForce(transform.right * moveForce);
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && jumps > 0)
         {
             // Impulse = "immediate change", Force = "gradual change" -- velocity vs acceleration
             rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            jumps--;
         }
 
+        // Don't need to clamp vertical velocity since jumping is an instantaneous change (jump force == max force)
         float vx = Mathf.Clamp(rb.velocity.x, -xSpeedMax, xSpeedMax);
         rb.velocity = new Vector2(vx, rb.velocity.y);
-
-        // Don't need to clamp vertical velocity since jumping is an instantaneous change (jump force == max force)
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.name);
+        jumps = jumpCount;
     }
 }
