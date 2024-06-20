6f60374ca93d3a26588ec411c6111ebcc41a932f
Smiley98 <smileysofficialcinema@gmail.com>
2024-06-20 10:59:04 -0400

    Added physics materials

--- Assets/Scripts/LE4/Player.cs
+++ Assets/Scripts/LE4/Player.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    const float moveForce = 10.0f;
+    const float jumpForce = 10.0f;
+
+    // Future improvement: air-speed vs ground-speed
+    const float xSpeedMax = 10.0f;
+    const float ySpeedMax = 10.0f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-transform.right * moveForce);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * moveForce);
+        }
+
+
     }
 }
