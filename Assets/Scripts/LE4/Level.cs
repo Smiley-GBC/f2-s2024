82e87d3443e7dd575a49bb7871f2957c4d7f23ea
Smiley98 <smileysofficialcinema@gmail.com>
2024-07-04 11:39:11 -0400

    Added level 1 read test

--- Assets/Scripts/LE4/Level.cs
+++ Assets/Scripts/LE4/Level.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Level : MonoBehaviour
@@ -28,28 +29,34 @@ public class Level : MonoBehaviour
 
     void Start()
     {
-        float x = 0.5f;
-        float y = 10.0f - 0.5f;
-
-        for (int row = 0; row < rowCount; row++)
+        using (StreamReader reader = new StreamReader("Assets/Levels/Level1.txt"))
         {
-            List<GameObject> tileObjectRow = new List<GameObject>();
-            for (int col = 0; col < colCount; col++)
-            {
-                GameObject tile = Instantiate(tilePrefab);
-                tile.transform.position = new Vector3(x + col, y);
-
-                int value = tileTypes[row, col];
-                Color color = value == 0 ? Color.gray : Color.white;
-                bool collision = value != 0;
-                tile.GetComponent<Collider2D>().enabled = collision;
-                tile.GetComponent<SpriteRenderer>().color = color;
-
-                tileObjectRow.Add(tile);
-            }
-            tileObjects.Add(tileObjectRow);
-            y -= 1.0f;
+            string line = reader.ReadLine();
+            Debug.Log(line);
         }
+
+        //float x = 0.5f;
+        //float y = 10.0f - 0.5f;
+        //
+        //for (int row = 0; row < rowCount; row++)
+        //{
+        //    List<GameObject> tileObjectRow = new List<GameObject>();
+        //    for (int col = 0; col < colCount; col++)
+        //    {
+        //        GameObject tile = Instantiate(tilePrefab);
+        //        tile.transform.position = new Vector3(x + col, y);
+        //
+        //        int value = tileTypes[row, col];
+        //        Color color = value == 0 ? Color.gray : Color.white;
+        //        bool collision = value != 0;
+        //        tile.GetComponent<Collider2D>().enabled = collision;
+        //        tile.GetComponent<SpriteRenderer>().color = color;
+        //
+        //        tileObjectRow.Add(tile);
+        //    }
+        //    tileObjects.Add(tileObjectRow);
+        //    y -= 1.0f;
+        //}
     }
 
     void Update()
