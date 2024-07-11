92ed5c4ea63edc40902e7e18ed66df1d8067d9f4
Smiley98 <smileysofficialcinema@gmail.com>
2024-07-10 22:50:25 -0400

    Removed unused assets

--- Assets/Scripts/LE4/Player.cs
+++ Assets/Scripts/LE4/Player.cs
@@ -5,115 +5,80 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
+    const float groundDrag = 0.25f;
+    const float airDrag = 0.5f;
 
-    const float moveForce = 15.0f;
-    const float jumpForce = 10.0f;
+    // Horizontal movement
+    const float xMoveAcc = 15.0f;
     const float xSpeedMax = 10.0f;
+    float xAcc = 0.0f;
 
+    // Vertical movement
+    const float yJumpSpeed = 10.0f;
     const int jumpCount = 2;
-    int jumps = jumpCount;
-    int levelLayer;
-
-    Vector2 previousVelocity = Vector2.zero;
+    int jumps = 0;
+    bool grounded = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        levelLayer = LayerMask.NameToLayer("Level");
     }
 
     void Update()
     {
-        // Horizontal movement
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.AddForce(new Vector2(moveInput * moveForce, 0));
-
-        // Jumping
+        xAcc = xMoveAcc * Input.GetAxisRaw("Horizontal");
         if (Input.GetKeyDown(KeyCode.W) && jumps > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 0); // Clear vertical velocity before jumping
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, yJumpSpeed);
             jumps--;
         }
     }
 
     void FixedUpdate()
     {
-        float clampedVelocityX = Mathf.Clamp(rb.velocity.x, -xSpeedMax, xSpeedMax);
-        rb.velocity = new Vector2(clampedVelocityX, rb.velocity.y);
-        previousVelocity = rb.velocity;
+        float dt = Time.fixedDeltaTime;
+        float yAcc = grounded ? 0.0f : Physics2D.gravity.y;
+        rb.velocity += new Vector2(xAcc, yAcc) * dt;
+
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -xSpeedMax, xSpeedMax), rb.velocity.y);
+        rb.velocity *= Mathf.Pow(grounded ? groundDrag : airDrag, dt);
     }
 
+    // Stop horizontal motion if x-collision, stop vertical motion if y-collision. If corner collision, maintain motion and resolve position.
     void OnCollisionEnter2D(Collision2D collision)
     {
-        //OnCollision(collision);
-    }
+        ContactPoint2D contact = collision.contacts[0];
 
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        OnCollision(collision);
-    }
-
-    // This is pretty jank... should probably switch to kinematic rigidbody but at that point why even use Unity Physics??
-    void OnCollision(Collision2D collision)
-    {
-        if (collision.gameObject.layer == levelLayer)
+        if (contact.normal.y == 0.0f)
         {
-            if (XCollision(collision))
-            {
-                rb.drag = 0.0f;
-                rb.totalForce = new Vector2(0.0f, rb.totalForce.y);
-                rb.velocity = new Vector2(rb.velocity.x, previousVelocity.y);
-            }
-            else
-            {
-                rb.drag = collision.gameObject.GetComponent<PhysicsData>().drag;
-            }
+            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+        }
+        else if (contact.normal.x == 0.0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0.0f);
+        }
+        else
+        {
+            // Don't do anything -- preserve motion & resolve position only in collision-stay.
         }
     }
 
-    void OnCollisionExit2D(Collision2D collision)
+    // Move player along "collision normal" via "minimum translation vector" every frame of collision to resolve!
+    void OnCollisionStay2D(Collision2D collision)
     {
-        // No drag if airborne
-        rb.drag = 0.0f;
+        ContactPoint2D contact = collision.contacts[0];
+        Vector2 mtv = contact.normal * Mathf.Abs(contact.separation);
+        rb.position += mtv;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == levelLayer)
-        {
-            // Reset jumps if grounded
-            jumps = jumpCount;
-        }
+        grounded = true;
+        jumps = jumpCount;
     }
 
-    bool XCollision(Collision2D collision)
+    void OnTriggerExit2D(Collider2D collision)
     {
-        List<ContactPoint2D> contacts = new List<ContactPoint2D>();
-        collision.GetContacts(contacts);
-        foreach (ContactPoint2D contact in contacts)
-        {
-            // Side collision if MTV is horizontal (no vertical component)
-            if (contact.normal.y == 0)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool YCollision(Collision2D collision)
-    {
-        List<ContactPoint2D> contacts = new List<ContactPoint2D>();
-        collision.GetContacts(contacts);
-        foreach (ContactPoint2D contact in contacts)
-        {
-            // Side collision if MTV is horizontal (no vertical component)
-            if (contact.normal.x == 0)
-            {
-                return true;
-            }
-        }
-        return false;
+        grounded = false;
     }
 }
