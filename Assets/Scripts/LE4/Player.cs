1931a8e886190d22a41b645b4f8bd166c0b63128
Smiley98 <smileysofficialcinema@gmail.com>
2024-07-18 10:43:29 -0400

    Finalized platformer physics for real this time!

--- Assets/Scripts/LE4/Player.cs
+++ Assets/Scripts/LE4/Player.cs
@@ -16,14 +16,15 @@ public class Player : MonoBehaviour
 
     const int jumpCount = 2;
     int jumps = 0;
+    bool grounded = false;
 
     // Limits
     const float xSpeedMax = 10.0f;
     const float ySpeedMax = 10.0f;
-
-    // TODO -- Add drag for something to do in class?
-    // Still not sure why Player slows down on its own...
-    // Try making a physics material with no friction and seeing what happens!
+    const float dragAir = 0.25f;    // Decelerate slower in air
+    const float dragGround = 0.05f; // Decelerate faster on ground
+    // TODO -- Add more nuanced drag based on direction of motion.
+    // For example, decrease air drag if falling down!
 
     void Start()
     {
@@ -45,16 +46,22 @@ public class Player : MonoBehaviour
     {
         float dt = Time.fixedDeltaTime;
         float dx = xAcc * dt;
-        float dy = 0.0f; // <-- in case we want custom vertical acceleration.
-        // Best to let Unity Rigidbody2D physics handle vertical acceleration for now. 
-        //float dy = Physics2D.gravity.y * rb.gravityScale;
+        float dy = 0.0f; // <-- no custom vertical acceleration.
         float xMax = Mathf.Clamp(rb.velocity.x + dx, -xSpeedMax, xSpeedMax);
         float yMax = Mathf.Clamp(rb.velocity.y + dy, -ySpeedMax, ySpeedMax);
+        float drag = grounded ? dragGround : dragAir;
         rb.velocity = new Vector2(xMax, yMax);
+        rb.velocity *= Mathf.Pow(drag, dt);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         jumps = jumpCount;
+        grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        grounded = false;
     }
 }
