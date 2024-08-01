2a3a7cae4bf310ee7c38fe58567b9b848d34d100
Smiley98 <smileysofficialcinema@gmail.com>
2024-08-01 10:46:41 -0400

    Fixed animation frames

--- Assets/Scripts/LE5/AnimationTester.cs
+++ Assets/Scripts/LE5/AnimationTester.cs
@@ -26,24 +26,64 @@ public class SpriteAnimation
 
 public class AnimationTester : MonoBehaviour
 {
+    SpriteAnimation walkLeft = new SpriteAnimation();
+    SpriteAnimation walkRight = new SpriteAnimation();
+    SpriteAnimation walkUp = new SpriteAnimation();
     SpriteAnimation walkDown = new SpriteAnimation();
 
     void Start()
     {
-        Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/characters");
+        walkLeft.renderer = GetComponent<SpriteRenderer>();
+        walkRight.renderer = GetComponent<SpriteRenderer>();
+        walkUp.renderer = GetComponent<SpriteRenderer>();
         walkDown.renderer = GetComponent<SpriteRenderer>();
+
+        // Time (seconds) per frame!
+        walkLeft.frameTime.total = 0.25f;
+        walkRight.frameTime.total = 0.25f;
+        walkUp.frameTime.total = 0.25f;
+        walkDown.frameTime.total = 0.25f;
+
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/characters");
+
+        walkLeft.frames.Add(sprites[13]);
+        walkLeft.frames.Add(sprites[12]);
+        walkLeft.frames.Add(sprites[13]);
+        walkLeft.frames.Add(sprites[14]);
+
+        walkRight.frames.Add(sprites[25]);
+        walkRight.frames.Add(sprites[24]);
+        walkRight.frames.Add(sprites[25]);
+        walkRight.frames.Add(sprites[26]);
+
+        walkUp.frames.Add(sprites[37]);
+        walkUp.frames.Add(sprites[36]);
+        walkUp.frames.Add(sprites[37]);
+        walkUp.frames.Add(sprites[38]);
 
         walkDown.frames.Add(sprites[1]);
         walkDown.frames.Add(sprites[0]);
         walkDown.frames.Add(sprites[1]);
         walkDown.frames.Add(sprites[2]);
-
-        // Time (seconds) per frame!
-        walkDown.frameTime.total = 0.25f;
     }
 
     void Update()
     {
-        walkDown.Update(Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+        {
+            walkUp.Update(Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            walkDown.Update(Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            walkLeft.Update(Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            walkRight.Update(Time.deltaTime);
+        }
     }
 }
