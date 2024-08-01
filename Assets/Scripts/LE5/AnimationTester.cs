0e95482c7a03a18ed493d6223a4fee9a4626e2b6
Smiley98 <smileysofficialcinema@gmail.com>
2024-08-01 11:27:29 -0400

    Finished animation system

--- Assets/Scripts/LE5/AnimationTester.cs
+++ Assets/Scripts/LE5/AnimationTester.cs
@@ -43,11 +43,7 @@ public class AnimationTester : MonoBehaviour
     State state = State.IDLE;
 
     // Frame 0 of each direction is the idle frame
-    SpriteAnimation walkLeft = new SpriteAnimation();
-    SpriteAnimation walkRight = new SpriteAnimation();
-    SpriteAnimation walkUp = new SpriteAnimation();
-    SpriteAnimation walkDown = new SpriteAnimation();
-
+    SpriteAnimation[] animations = new SpriteAnimation[4];
     SpriteRenderer renderer;
 
     void Start()
@@ -55,32 +51,18 @@ public class AnimationTester : MonoBehaviour
         renderer = GetComponent<SpriteRenderer>();
 
         // Time (seconds) per frame!
-        walkLeft.frameTime.total = 0.25f;
-        walkRight.frameTime.total = 0.25f;
-        walkUp.frameTime.total = 0.25f;
-        walkDown.frameTime.total = 0.25f;
+        for (int i = 0; i < animations.Length; i++)
+        {
+            animations[i] = new SpriteAnimation();
+            animations[i].frameTime.total = 0.25f;
+        }
 
         Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/characters");
 
-        walkLeft.frames.Add(sprites[13]);
-        walkLeft.frames.Add(sprites[12]);
-        walkLeft.frames.Add(sprites[13]);
-        walkLeft.frames.Add(sprites[14]);
-
-        walkRight.frames.Add(sprites[25]);
-        walkRight.frames.Add(sprites[24]);
-        walkRight.frames.Add(sprites[25]);
-        walkRight.frames.Add(sprites[26]);
-
-        walkUp.frames.Add(sprites[37]);
-        walkUp.frames.Add(sprites[36]);
-        walkUp.frames.Add(sprites[37]);
-        walkUp.frames.Add(sprites[38]);
-
-        walkDown.frames.Add(sprites[1]);
-        walkDown.frames.Add(sprites[0]);
-        walkDown.frames.Add(sprites[1]);
-        walkDown.frames.Add(sprites[2]);
+        animations[0].frames = CreateAnimationFrames(sprites, 12);
+        animations[1].frames = CreateAnimationFrames(sprites, 24);
+        animations[2].frames = CreateAnimationFrames(sprites, 36);
+        animations[3].frames = CreateAnimationFrames(sprites, 0);
     }
 
     void Update()
@@ -114,54 +96,33 @@ public class AnimationTester : MonoBehaviour
         }
         else
         {
+            state = Input.GetKey(KeyCode.LeftShift) ? State.RUN : State.WALK;
+            float frameTime = state == State.RUN ? 0.05f : 0.25f;
+            for (int i = 0; i < animations.Length; i++)
+            {
+                animations[i].frameTime.total = frameTime;
+            }
             OnUpdate();
         }
     }
 
+    List<Sprite> CreateAnimationFrames(Sprite[] sprites, int index)
+    {
+        List<Sprite> frames = new List<Sprite>();
+        frames.Add(sprites[index + 1]);
+        frames.Add(sprites[index + 0]);
+        frames.Add(sprites[index + 1]);
+        frames.Add(sprites[index + 2]);
+        return frames;
+    }
+
     void OnIdle()
     {
-        Sprite sprite = null;
-        switch (direction)
-        {
-            case Direction.LEFT:
-                sprite = walkLeft.frames[0];
-                break;
-
-            case Direction.RIGHT:
-                sprite = walkRight.frames[0];
-                break;
-
-            case Direction.UP:
-                sprite = walkUp.frames[0];
-                break;
-
-            case Direction.DOWN:
-                sprite = walkDown.frames[0];
-                break;
-        }
-        renderer.sprite = sprite;
+        renderer.sprite = animations[(int)direction].frames[0];
     }
 
     void OnUpdate()
     {
-        float dt = Time.deltaTime;
-        switch (direction)
-        {
-            case Direction.LEFT:
-                walkLeft.Update(renderer, dt);
-                break;
-
-            case Direction.RIGHT:
-                walkRight.Update(renderer, dt);
-                break;
-
-            case Direction.UP:
-                walkUp.Update(renderer, dt);
-                break;
-
-            case Direction.DOWN:
-                walkDown.Update(renderer, dt);
-                break;
-        }
+        animations[(int)direction].Update(renderer, Time.deltaTime);
     }
 }
